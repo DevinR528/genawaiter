4ac34a69e9e5c5fd9ebb8f42af765bc1f4f56573
Devin R <devin.ragotzy@gmail.com>
2019-11-15 21:35:02 -0500

    paste tests

--- tests/stack.rs
+++ tests/stack.rs
@@ -8,18 +8,48 @@ use genawaiter::{
     stack::{Co, Gen, Shelf},
 };
 
+use futures::{
+    executor::{block_on, block_on_stream},
+    stream::{Stream, StreamExt},
+};
+
 async fn odd_numbers_less_than_ten(co: Co<'_, i32>) {
     for n in (1..).step_by(2).take_while(|&n| n < 10) {
+        println!("back to stack frame");
         co.yield_(n).await;
     }
 }
 
+// async fn stream1() -> &'static impl Stream {
+//     generator_mut!(gen, odd_numbers_less_than_ten);
+//     gen
+// }
+
 #[test]
-fn test_basic() {
+fn test_basic_stack() {
     generator_mut!(gen, odd_numbers_less_than_ten);
 
     let xs: Vec<_> = gen.into_iter().collect();
     assert_eq!(xs, [1, 3, 5, 7, 9]);
+}
+
+#[test]
+fn test_basic_stream() {
+    generator_mut!(gen, odd_numbers_less_than_ten);
+    let mut x = gen.take(3);
+    block_on(async {
+        println!("{:?}", x.next().await);
+        println!("{:?}", x.next().await);
+    });
+    // block_on(async { println!("{:?}", gen.next().await) });
+
+    // gen.take(3);
+
+    // gen.for_each(|x| println!("{:?}", x));
+
+    // block_on_stream(s);
+
+    // assert_eq!(xs, [1, 3, 5, 7, 9]);
 }
 
 #[test]
